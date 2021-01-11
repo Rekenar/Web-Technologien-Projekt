e653691b08a43787e7729c09071f84deca02eb74
Rekenar <58118852+Rekenar@users.noreply.github.com>
2021-01-11 16:30:20 +0100

    Initial commit

---
+++ Project/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module'
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { StartComponent } from './start/start.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule } from '@angular/material/button'
+import {MatFormFieldModule } from '@angular/material/form-field'
+import {MatCardModule } from '@angular/material/card'
+import {MatInputModule } from '@angular/material/input'
+import {MatToolbarModule } from '@angular/material/toolbar';
+import { OverviewComponent } from './overview/overview.component';
+import { HomeComponent } from './home/home.component'
+import {MatTableModule}from '@angular/material/table'
+import {MatSortModule}from '@angular/material/sort'
+import {MatPaginatorModule}from '@angular/material/paginator'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    StartComponent,
+    OverviewComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Project/src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common'; 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor( private location: Location )
+        {}
+
+  ngOnInit(): void {
+    this.location.replaceState("/app-login");
+  }
+}

1282751605dd6544353ef3ee53c5bd4871b6b138
Rekenar <58118852+Rekenar@users.noreply.github.com>
2021-01-16 19:03:28 +0100

    accountid zu userid

--- API/app.js
+++ API/app.js
@@ -6,15 +6,15 @@ const db = require('../db')
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Credentials", "true");
-		res.setHeader("Access-Control-Allow-Headers", "content-type");
-		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  next();
 });
 
 app.get('/list', (req, res, next) => {
-  db.query('SELECT * FROM spending WHERE accountid = $1', [3], (err, resp) => {
+  db.query('SELECT * FROM spending WHERE userid = $1', [3], (err, resp) => {
     if (err) {
       return next(err)
     }
@@ -23,5 +23,5 @@ app.get('/list', (req, res, next) => {
 })
 
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 })

1f1a7c639479bf6f45ab47b9bb5df5cc9c67ea06
Rekenar <58118852+Rekenar@users.noreply.github.com>
2021-01-24 19:21:38 +0100

    kleine Änderungen

--- Project/src/app/core/task.service.ts
+++ Project/src/app/core/task.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { List } from '../models/list.model';
-import { LoginRequestDTO } from '../models/LoginRequestDTO.model';
+import { AccountRequestDTO } from '../models/AccountRequestDTO.model';
 import { Total } from '../models/total.model';
 
 @Injectable({
@@ -15,24 +15,23 @@ export class TaskService {
   constructor(private http: HttpClient) { }
 
   private listUrl = 'http://localhost:3000/spending';
-  private overallUrl = 'http://localhost:3000/overall';
   private accountUrl = "http://localhost:3000/account";
+  private registerUrl = "http://localhost:3000/register";
 
   getList(): Observable<List[]> {
     return this.http.get<List[]>(this.listUrl);
   }
-  getOverall(){
-    return this.http.get<Total[]>(this.overallUrl)
-  }
-
   deleteEntry(position:Number) {
     return this.http.delete(this.listUrl+'/'+position);
   }
-  addEntry(list: List){
+  addEntry(list: List) {
     return this.http.post(this.listUrl, list);
   }
 
-  login(request:LoginRequestDTO):Observable<any>{
-    return this.http.post<any>(this.accountUrl,request);
+  login(request: AccountRequestDTO): Observable<any> {
+    return this.http.post<any>(this.accountUrl, request);
+  }
+  register(request: AccountRequestDTO): Observable<any> {
+    return this.http.post<any>(this.registerUrl, request);
   }
 }
